bcb5790120961731fc4b35051a0cf67884155e29
JodieFlacko <96775468+JodieFlacko@users.noreply.github.com>
2025-03-27 11:35:39 +0100

    style: location tab

--- src/contacts-load.js
+++ src/contacts-load.js
@@ -8,20 +8,22 @@ export function contactsLoad(content){
   const contactItems = [
     {
       title: "How can we help you?",
-      link: "Send a mail",
-      desc: "For generic information\nrandom@mail.com",
+      link: " Send a mail",
+      href: "mailto: random@mail.com"
     },
 
     {
-      title: "Call us",
+      title: "Call us:",
       link: "+39 387 552 581",
-      desc: "Fell free to reach for us",
+      href: "tel: +39 387 552 581",
+      desc: "Fell free to reach for us.",
     },
 
     {
-      title: "How to get here",
+      title: "How to get here:",
       link: "Location",
-      desc: "Directions to get here",
+      href: "https://www.google.com/maps/dir//Via+Castel+Cicala,+80035+Nola+NA/@40.9206686,14.5453948,1642m/data=!3m2!1e3!4b1!4m9!4m8!1m0!1m5!1m1!1s0x133bb3e4504a3a4f:0xc428584518e78525!2m2!1d14.5505447!2d40.9206606!3e2?entry=ttu&g_ep=EgoyMDI1MDMxMi4wIKXMDSoJLDEwMjExNDUzSAFQAw%3D%3D",
+      desc: "Ring the bell and the goblin will open the door for you.",
     },
   ];
   
@@ -30,15 +32,32 @@ export function contactsLoad(content){
     const title = document.createElement("span");
     const link = document.createElement("a");
     const desc = document.createElement("p");
+    const strong = document.createElement("strong");
+    
 
     title.textContent = item.title;
     link.textContent = item.link;
+    if(item.link === "Location") setLinkAttributes(item.href, link);
+    else link.setAttribute("href", item.href);
     desc.textContent = item.desc;
 
-    itemDiv.appendChild(title);
+    itemDiv.appendChild(strong)
+    strong.appendChild(title);
     itemDiv.appendChild(link);
     itemDiv.appendChild(desc);
 
     container.appendChild(itemDiv);
+  };
+}
+
+function setLinkAttributes(href, element){
+  const attributes = {
+    href,
+    target: "_blank",
+    rel: "noopener noreferrer",
+  };
+
+  for(let property in attributes){
+    element.setAttribute(property, attributes[property]);
   };
 }
